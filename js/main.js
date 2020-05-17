bd1522a95d4efdc3b17d0cda1717b63549b3e856
Hunter Brown <hunterbrownmusic1@gmail.com>
2020-05-17 13:19:00 -0500

    test_commit_unfinished

--- js/main.js
+++ js/main.js
@@ -1,14 +1,24 @@
+/*
+A piece of artware that takes the user's locataion data and creates a
+generative audio/visual composition.
+
+locataion data is parsed in js/getData.js
+*/
+
+/*___________________________________________________________________________*/
+
+//Globals: audio context, fft, fft canvas
 const ctx = new (window.AudioContext || window.webkitAudioContext)()
 const fft = new AnalyserNode(ctx, { fftSize: 2048 })
-createWaveCanvas({ element: 'section', analyser: fft })
 
+//createWaveCanvas is causing an error
+// createWaveCanvas({ element: 'section', analyser: fft })
 
-/*
-
-  const tone = new OscillatorNode(ctx)
-  tone.connect(fft)
-  tone.connect(ctx.destination)
-  tone.start(ctx.currentTime)
-  tone.stop(ctx.currentTime + 1)
-
-*/
+/*___ Data -> Sound _________________________________________________________*/
+console.log(chars)
+console.log(info)
+console.log(nums)
+console.log(numsSplitNorm)
+console.log(numsNorm)
+console.log(numsSplitBipolar)
+console.log(numsNormBipolar)
