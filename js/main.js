0b9e28212f388b772ff86377b0d36f1b51e443ae
Hunter Brown <hunterbrownmusic1@gmail.com>
2020-05-19 17:31:31 -0500

    reverse_fix

--- js/main.js
+++ js/main.js
@@ -47,81 +47,8 @@ for (var i = 0; i < atks.length; i++) {
 return atksDiv
 }
 
-//______ Generate array of chars for decoding into numbers ________
-let chars = [], c0 = ' '.charCodeAt(0),  cN = '~'.charCodeAt(0);
-for (; c0 <= cN; ++c0) {chars.push(String.fromCharCode(c0))}
-
-/*______Get Location and Internet Provider Info and store into an array_______*/
-//init ip-api to retrieve data
-const endpoint = 'https://ip-api.com/json/?fields=country,countryCode,city,zip,lat,lon,org,query';
-
-//function that "gets" the user's info, returns data as an array when called
-function getInfo () {
-  let data = []
-  const xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      var r = JSON.parse(this.responseText);
-      data = [r.countryCode,r.city,r.zip,String(r.lat),String(r.lon),r.org,r.query]
-    }
-  }
-  xhr.open('GET', endpoint, false) //must be synchronous!
-  xhr.send()
-  return data
-}
-
-//store getInfo(); to an array & array for storing char vaules
-const info = getInfo();
-let nums = []
-let numsNorm = []
-let numsNormBipolar = []
-let numsSplit = []
-let numsSplitNorm = []
-let numsSplitBipolar = []
-
-//Make empty multi-dim arrays to fill w/nums
-for (var t = 0; t < info.length; t++) numsSplit.push([])
-for (var n = 0; n < info.length; n++) numsSplitNorm.push([])
-for (var  b = 0; b < info.length; b++) numsSplitBipolar.push([0])
-
-//Store char vaules into split arrays and normalize (bipolar and standard)
-for (var j = 0; j < info.length; j++) {
-  for (var a = 0; a < info[j].length; a++) {
-    for (var p = 0; p < chars.length; p++) {
-     if (info[j][a] === chars[p]) {
-       numsSplit[j].push(p) //split Nums
-       numsSplitNorm[j].push(p/chars.length) //split nums 0.0-1.0
-
-       //if p < size of array, make negative else make positive (for wavetables)
-       if (chars[p] < (chars.length/2)) {numsSplitBipolar[j].push(p/chars.length)}
-        else {numsSplitBipolar[j].push((p/chars.length) * -1)}
-      }
-    }
-  }
-}
-
-//concat aboves arrays together into one array
-for (var y = 0; y < numsSplit.length; y++) nums = nums.concat(numsSplit[y])
-//nums normalized
-for (var m = 0; m < numsSplit.length; m++) {
-  numsNorm = numsNorm.concat(numsSplitNorm[m])
-}
-//nums normalized/bipolar
-for (var o = 0; o < numsSplitBipolar.length; o++) {
-  numsNormBipolar = numsNormBipolar.concat(numsSplitBipolar[o])
-}
-
-console.log(chars)
-console.log(info)
-console.log(nums)
-console.log(numsSplitNorm)
-console.log(numsNorm)
-console.log(numsNormBipolar)
-console.log(numsSplitBipolar)
-console.log(numsNormBipolar)
 
 //Make audio instances and canvas___________________________________________
-
 //Make oscs, lvls, and pans
 var count, oscs = [], lvls = [], pans = []
 let panPos = [-1, 0, 1]
